b9b6fef0c11b333dd0dd51d64c02c5df37f09d03
sachinmandloi06 <sachinmandloi06@gmail.com>
2024-09-19 19:39:27 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -126,7 +126,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/sachinmandloi06/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
